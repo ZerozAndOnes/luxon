a73ac59bc3b6d64bf251f173b75b8cd983b788ca
ZerozAndOnes <m.yaseen@sinc.business>
2020-02-07 20:51:32 +0300

    Chrome canary work around https://bugs.chromium.org/p/chromium/issues/detail?id=1025564&can=2&q=%2224%3A00%22%20datetimeformat from luxon https://github.com/ZerozAndOnes/luxon/commit/63122c7f74241db4bbccd1dcd0c7f70d8bec7579

--- src/zones/IANAZone.js
+++ src/zones/IANAZone.js
@@ -1,4 +1,9 @@
-import { parseZoneInfo, isUndefined, ianaRegex, objToLocalTS } from "../impl/util.js";
+import {
+  parseZoneInfo,
+  isUndefined,
+  ianaRegex,
+  objToLocalTS
+} from "../impl/util.js";
 import Zone from "../zone.js";
 
 const matchingRegex = RegExp(`^${ianaRegex.source}$`);
@@ -102,8 +107,20 @@ export default class IANAZone extends Zone {
       dtf = makeDTF(this.zoneName),
       [year, month, day, hour, minute, second] = dtf.formatToParts
         ? partsOffset(dtf, date)
-        : hackyOffset(dtf, date);
-    const asUTC = objToLocalTS({ year, month, day, hour, minute, second, millisecond: 0 });
+        : hackyOffset(dtf, date),
+      // work around https://bugs.chromium.org/p/chromium/issues/detail?id=1025564&can=2&q=%2224%3A00%22%20datetimeformat
+      adjustedHour = hour === 24 ? 0 : hour;
+
+    const asUTC = objToLocalTS({
+      year,
+      month,
+      day,
+      hour: adjustedHour,
+      minute,
+      second,
+      millisecond: 0
+    });
+
     let asTS = date.valueOf();
     asTS -= asTS % 1000;
     return (asUTC - asTS) / (60 * 1000);
